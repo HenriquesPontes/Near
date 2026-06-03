0efbd74293dd17936f5dd794596c06af4151a220
Henriques <pontes.18@hotmail.com>
2026-06-03 13:46:23 +0200

    Initial Commit

---
+++ Near/NearApp.swift
@@ -0,0 +1,32 @@
+//
+//  NearApp.swift
+//  Near
+//
+//  Created by Admin on 6/3/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NearApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
